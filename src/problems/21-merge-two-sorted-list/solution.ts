b2c9199e9f66e6c2407dc2408e1c847659a073ce
minidonut <nycom13@gmail.com>
2020-04-19 00:16:27 +0900

    Refactor 21-naive solution

--- src/problems/21-merge-two-sorted-list/solution.ts
+++ src/problems/21-merge-two-sorted-list/solution.ts
@@ -6,51 +6,37 @@ import { List, LinkedList } from "../../helpers";
  * Naive solution
  */
 const Solution = (l1: LinkedList, l2: LinkedList): LinkedList => {
-  let head, tail, l1Pointer, l2Pointer;
-  if (!l1) {
-    return l2;
-  }
-  if (!l2) {
-    return l1;
-  }
+  const head: any = { next: null };
+  let tail = head;
+  let l1Pointer = l1;
+  let l2Pointer = l2;
 
-  if (l1.val < l2.val) {
-    head = l1;
-    tail = l1;
-    l1Pointer = head.next;
-    l2Pointer = l2;
-  } else {
-    head = l2;
-    tail = l2;
-    l1Pointer = l1;
-    l2Pointer = l2.next;
-  }
-
-  // eslint-disable-next-line
-  while (true) {
-    if (!l1Pointer) {
+  while (l1Pointer !== null && l2Pointer !== null) {
+    if (l1Pointer.val > l2Pointer.val) {
       tail.next = l2Pointer;
-      break;
-    }
-    if (!l2Pointer) {
-      tail.next = l1Pointer;
-      break;
-    }
-
-    if (l1Pointer.val < l2Pointer.val) {
-      tail.next = l1Pointer;
-      tail = tail.next;
-      l1Pointer = l1Pointer.next;
-    } else {
-      tail.next = l2Pointer;
-      tail = tail.next;
       l2Pointer = l2Pointer.next;
+    } else {
+      tail.next = l1Pointer;
+      l1Pointer = l1Pointer.next;
     }
+    tail = tail.next;
   }
-  return head;
+  tail.next = l1Pointer === null ? l2Pointer : l1Pointer;
+  return head.next;
+};
+
+
+const Short = (l1: LinkedList, l2: LinkedList) => {
+  /**
+   * Short coding
+   */
+  const toArray = (head: LinkedList, arr: any[]): any[] => head ? (arr.push(head.val), toArray(head.next, arr)) : arr;
+  const toList = (arr: any[]): LinkedList => arr.length === 0 ? null : ({ val: arr[0], next: toList(arr.slice(1)) });
+  return toList([...toArray(l1, []), ...toArray(l2, [])].sort((a, b) => a - b));
 };
 
 export default {
   "default": Solution,
   validator: List.toString,
+  Short,
 };
