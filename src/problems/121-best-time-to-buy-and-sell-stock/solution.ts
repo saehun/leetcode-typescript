7e0990374e190bd434c9e376051d7c47012af978
minidonut <nycom13@gmail.com>
2020-04-18 21:55:35 +0900

    Solve 121

--- src/problems/121-best-time-to-buy-and-sell-stock/solution.ts
+++ src/problems/121-best-time-to-buy-and-sell-stock/solution.ts
@@ -1,6 +1,21 @@
+/**
+ * Naive solution = O(n^2)
+ */
 
+/**
+ * O(n) solution
+ */
 const maxProfit = (prices: number[]): number => {
-  return 0;
+  let profit = 0;
+  let buyAt = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] < buyAt) {
+      buyAt = prices[i];
+    } else {
+      profit = Math.max(profit, prices[i] - buyAt);
+    }
+  }
+  return profit;
 };
 
 export default {
