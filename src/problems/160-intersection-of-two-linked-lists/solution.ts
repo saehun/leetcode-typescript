436bbbe644d9fb37d70f4403489caeb939a567b9
minidonut <nycom13@gmail.com>
2020-04-22 04:16:47 +0900

    Sovle 160 with naive solution

--- src/problems/160-intersection-of-two-linked-lists/solution.ts
+++ src/problems/160-intersection-of-two-linked-lists/solution.ts
@@ -1,23 +1,36 @@
 import { LinkedList } from "../../helpers/list";
 
-const Solution = (headA: LinkedList, headB: LinkedList): LinkedList => {
+
+/**
+ * Naive solution
+ */
+const NaiveSolution = (headA: LinkedList, headB: LinkedList): LinkedList => {
   const map: any = {};
   let cursorA = headA;
   while (cursorA) {
-    map[cursorA.val] = true;
+    if (map[cursorA.val]) {
+      map[cursorA.val].push(cursorA);
+    } else {
+      map[cursorA.val] = [cursorA];
+    }
     cursorA = cursorA.next;
   }
 
   let cursorB = headB;
   while (cursorB) {
-    if (map[cursorB.val]) return cursorB;
+    if (map[cursorB.val] && map[cursorB.val].includes(cursorB)) return cursorB;
     cursorB = cursorB.next;
   }
 
   return null;
 };
 
+const Solution = (headA: LinkedList, headB: LinkedList): LinkedList => {
+  return null;
+};
+
 export default {
   "default": Solution,
+  NaiveSolution,
   validator: (x: LinkedList) => x.val,
 };
