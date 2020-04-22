f099bb8163c78970898c01bc5da8ae928d3a350e
minidonut <nycom13@gmail.com>
2020-04-22 18:19:09 +0900

    Sovle 448 using set(quite simple)

--- src/problems/448-find-all-numbers-disappeared-in-an-array/solution.ts
+++ src/problems/448-find-all-numbers-disappeared-in-an-array/solution.ts
@@ -1,4 +1,10 @@
-const Solution = () => { };
+const Solution = (nums: number[]): number[] => {
+  const set = new Set<number>();
+  for (let i = 0; i < nums.length; set.add(++i)) { }
+  for (let i = 0; i < nums.length; set.delete(nums[i++])) { }
+
+  return Array.from(set);
+};
 
 export default {
   "default": Solution,
