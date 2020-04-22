847de43e042dc66748dcc20bffe17cf4b6d48ace
minidonut <nycom13@gmail.com>
2020-04-22 18:59:09 +0900

    Sovle 448 using Inplace(quite genius)

--- src/problems/448-find-all-numbers-disappeared-in-an-array/solution.ts
+++ src/problems/448-find-all-numbers-disappeared-in-an-array/solution.ts
@@ -1,12 +1,34 @@
 const Solution = (nums: number[]): number[] => {
   const set = new Set<number>();
-  for (let i = 0; i < nums.length; set.add(++i)) { }
-  for (let i = 0; i < nums.length; set.delete(nums[i++])) { }
+  for (let i = 0; i < nums.length; set.add(++i)) { /* do nothing */ }
+  for (let i = 0; i < nums.length; set.delete(nums[i++])) { /* do nothing */ }
 
   return Array.from(set);
 };
+
+const Inplace = (nums: number[]): number[] => {
+  for (let i = 0; i < nums.length; i++) {
+    const targetIndex = Math.abs(nums[i]) - 1;
+    if (nums[targetIndex] > 0) {
+      nums[targetIndex] = -nums[targetIndex];
+    }
+  }
+
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+  return result;
+};
+
 
 export default {
   "default": Solution,
   validator: (x: any) => x,
 };
+
+/**
+ *
+ */
