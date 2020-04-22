25a7ad4723eb896e3b0cbf1a0e5616dfc9d0bf25
minidonut <nycom13@gmail.com>
2020-04-22 15:57:44 +0900

    Better javascript solution for 283

--- src/problems/283-move-zeros/solution.ts
+++ src/problems/283-move-zeros/solution.ts
@@ -1,26 +1,16 @@
 const Solution = (nums: number[]): void => {
-  let nonZeroIndex = 0;
-  const swap = (a: number, b: number) => {
-    const tmp = nums[a];
-    nums[a] = nums[b];
-    nums[b] = tmp;
-  };
-
+  let count = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      if (i === nonZeroIndex) {
-        nonZeroIndex++;
-      } else {
-        for (let j = nonZeroIndex; j < nums.length; j++) {
-          if (nums[j] === 0) {
-            swap(i, j);
-            break;
-          }
-        }
-      }
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      count++;
+      i--;
     }
   }
 
+  for (let i = 0; i < count; i++) {
+    nums.push(0);
+  }
 };
 
 export default {
