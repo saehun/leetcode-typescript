85d30a5efd50704ec3ea7ac19e8044bec6e916c2
minidonut <nycom13@gmail.com>
2020-04-23 23:49:22 +0900

    Improve solution#3

--- src/problems/3-longest-substring-without-repeating-characters/case.ts
+++ src/problems/3-longest-substring-without-repeating-characters/case.ts
@@ -4,4 +4,7 @@ export default [
   { input: ["pwwkew"], output: 3 },
   { input: [" "], output: 1 },
   { input: ["dvdf"], output: 3 },
+  { input: ["aabaab!bb"], output: 3 },
+  { input: ["gaaqfeqlqky"], output: 4 },
+  { input: ["abcddddefgh"], output: 5 },
 ];
--- src/problems/3-longest-substring-without-repeating-characters/solution.ts
+++ src/problems/3-longest-substring-without-repeating-characters/solution.ts
@@ -1,21 +1,42 @@
+import chalk from "chalk";
+const log = (chars: string[], left: number, right: number) => {
+  const arr = chars.map((c, i) => {
+    if (i === left) {
+      if (i === right) {
+        return chalk.magenta(c);
+      } else {
+        return chalk.red(c);
+      }
+    } else if (i === right) {
+      return chalk.blueBright(c);
+    } else {
+      return c;
+    }
+  }).join(",");
+  console.log(`[${arr}]`);
+};
+
 const Solution = (s: string): number => {
   if (s === "") return 0;
-  const pointer: any = { startAt: 0, length: 0, contains: {} };
-  let max = 0;
-  const characters = s.split("");
-
-  for (let i = 0; i < characters.length; i++) {
-    const char = characters[i];
-    if (pointer.contains[char]) {
-      i = pointer.startAt + 1;
-      pointer.startAt = i;
-      pointer.length = 1;
-      pointer.contains = { [characters[pointer.startAt]]: true };
+  const chars = s.split("");
+  const contains: any = {};
+  let max = 0, left = 0, right = 0;
+  for (; right < chars.length; right++) {
+    const char = chars[right];
+    if (contains[char]) {
+      while (left <= right) {
+        if (chars[left] === char) {
+          left++;
+          break;
+        }
+        contains[chars[left]] = false;
+        left++;
+      }
     } else {
-      pointer.length++;
-      pointer.contains[char] = true;
+      contains[char] = true;
     }
-    max = Math.max(max, pointer.length);
+    max = Math.max(max, right - left + 1);
+    /* log(chars, left, right); */
   }
   return max;
 };
