a2812cbdc66bca996d69c2bc863742a9b5e80963
minidonut <nycom13@gmail.com>
2020-04-23 17:16:55 +0900

    problem#15 (Improved, time limit exceeded yet.)

--- src/problems/15-3sum/solution.ts
+++ src/problems/15-3sum/solution.ts
@@ -1,6 +1,5 @@
 const Solution = (nums: number[]): number[][] => {
   const cache: any = {};
-  const set = new Set();
   for (let i = 0; i < nums.length; i++) {
     for (let j = i + 1; j < nums.length; j++) {
       const key = nums[i] + nums[j];
@@ -12,20 +11,21 @@ const Solution = (nums: number[]): number[][] => {
     }
   }
 
+  const resultMap: any = {};
   for (let i = 0; i < nums.length; i++) {
     const candidates = cache[-nums[i]];
     if (!candidates) continue;
 
     for (const arr of candidates) {
       if (!arr.includes(i)) {
-        set.add(
-          [...arr, i].map((x: number) => nums[x]).sort((a, b) => a - b).join("_")
-        );
+        const tmp = [nums[arr[0]], nums[arr[1]], nums[i]];
+        tmp.sort((a, b) => a - b);
+        resultMap[tmp.join(" ")] = tmp;
       }
     }
   }
 
-  return Array.from(set).map((x: any) => x.split("_").map((x: any) => Number(x)));
+  return Object.values(resultMap);
 };
 
 export default {
