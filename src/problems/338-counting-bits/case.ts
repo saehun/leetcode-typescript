2eee94b7074054776adc3685f3a478615187728d
minidonut <nycom13@gmail.com>
2020-04-30 22:25:16 +0900

    Solve 338 at O(n) time, O(n) space using generator

--- src/problems/338-counting-bits/case.ts
+++ src/problems/338-counting-bits/case.ts
@@ -1,3 +1,6 @@
 export default [
-  { input: [0], output: true },
+  { input: [2], output: [0, 1, 1] },
+  { input: [5], output: [0, 1, 1, 2, 1, 2] },
+  { input: [8], output: [0, 1, 1, 2, 1, 2, 2, 3, 1] },
+  { input: [9], output: [0, 1, 1, 2, 1, 2, 2, 3, 1, 2] },
 ];
--- src/problems/338-counting-bits/solution.ts
+++ src/problems/338-counting-bits/solution.ts
@@ -1,6 +1,60 @@
-const Solution = () => { };
+
+/*
+0 = 0   ->     0
+1 = 1   ->     1
+2 = 10  ->     1
+3 = 11  ->     2
+4 = 100 ->     1
+5 = 101 ->     2
+6 = 110 ->     2
+7 = 111 ->     3
+8 = 1000 ->    1
+9 = 1001 ->    2  // 8+1
+...
+16 = 10000 ->  1
+
+
+[4..7]   -> 1 + [0..3]
+[8..15]  -> 1 + [0..7]
+[16..31] -> 1 + [0..15]
+...
+[2^n ... 2^n-1] -> 1 + [0...2^-1]
+*/
+
+function* countBits() {
+  yield 0;
+  yield 1;
+
+  let index = 2;
+  let lastSquareNumber = 1;
+  const result = [0, 1];
+
+  // eslint-disable-next-line
+  while (true) {
+    if (index == lastSquareNumber * 2) {
+      lastSquareNumber *= 2;
+    }
+    result[index] = result[index - lastSquareNumber] + 1;
+    yield result[index];
+    index++;
+  }
+}
+
+const take = <T extends unknown>(num: number, generator: () => Generator<T, any, boolean>): T[] => {
+  const arr: T[] = [];
+  const iter = generator();
+  for (let i = 0; i < num; i++) {
+    arr.push(iter.next().value);
+  }
+
+  return arr;
+};
+
+const Solution = (num: number): number[] => {
+  return take(num + 1, countBits);
+};
 
 export default {
   "default": Solution,
-  validator: (x: any) => x,
+  validator: (x: any) => x.join(""),
 };
