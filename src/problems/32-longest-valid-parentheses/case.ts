14eb7bc3ef6a8826c40a39d90956edf88c9d60d3
minidonut <nycom13@gmail.com>
2020-05-01 05:46:42 +0900

    Solve problem 32

--- src/problems/32-longest-valid-parentheses/case.ts
+++ src/problems/32-longest-valid-parentheses/case.ts
@@ -2,7 +2,28 @@ export default [
   { input: ["(()"], output: 2 },
   { input: [")()())"], output: 4 },
   { input: [")(()())"], output: 6 },
-  { input: ["()(()"], output: 2 },
-  { input: [")()(((())))("], output: 10 },
-  { input: ["(())()(()(("], output: 6 },
+  { input: ["()(()"], output: 2 },         // )(()가 valid로 나오는 경우
+  { input: [")()(((())))("], output: 10 }, // 마지막에 ( 가 나오는 경우
+  { input: ["(())()(()(("], output: 6 },   // backtracking을 해보았지만 ())()(() 가 valid로 나오는 경우
 ];
+
+//
+//
+//   (
+// ( ( (   (
+//--------------
+// ( ( ) ) ( ) )
+// x x 1 0 x 0 x
+// x x 2 4 x 6 x
+// o o o o o o x
+
+
+//
+//
+//
+// (   (     (
+//--------------
+// ( ) ( ) ) ( )
+// x 0 x 2 x x 5
+// x 2 x 2 x x 2
+// o o o o x o o
--- src/problems/32-longest-valid-parentheses/solution.ts
+++ src/problems/32-longest-valid-parentheses/solution.ts
@@ -1,44 +1,36 @@
 
 
 const Solution = (s: string): number => {
-  let max = 0;
-  let leftCount = 0;
-  let rightCount = 0;
-  let lastIndex = 0;
+  const stack = [];
+  const check = Array(s.length).fill(false);
 
   for (let i = 0; i < s.length; i++) {
     const char = s[i];
-    if (char === ")") rightCount++;
-    if (char === "(") leftCount++;
-
-    if (rightCount > leftCount) {
-      max = Math.max(max, leftCount + rightCount - 1);
-      leftCount = 0;
-      rightCount = 0;
-      lastIndex = i + 1;
+    if (char === ")") {
+      if (stack.length === 0) {
+        continue;
+      } else {
+        const matched = stack.pop();
+        check[matched] = true;
+        check[i] = true;
+      }
+    } else {
+      stack.push(i);
     }
-    console.log(`i=${i} char=${s[i]} leftCount=${leftCount} rightCount=${rightCount} max=${max} lastIndex=${lastIndex}`);
   }
 
-  console.log("----------");
-  for (let i = s.length - 1; i > lastIndex - 1; i--) {
-    const char = s[i];
-    if (char === ")") break;
-    leftCount--;
-    console.log(`i=${i} char=${s[i]} leftCount=${leftCount} rightCount=${rightCount} max=${max} lastIndex=${lastIndex}`);
+  let max = 0;
+  let curMax = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (check[i]) {
+      curMax++;
+    } else {
+      max = Math.max(max, curMax);
+      curMax = 0;
+    }
   }
 
-  for (let i = lastIndex; i < s.length; i++) {
-    const char = s[i];
-    if (leftCount === rightCount && s[i] === "(") break;
-    if (char === ")") rightCount--;
-    if (char === "(") leftCount--;
-    console.log(`i=${i} char=${s[i]} leftCount=${leftCount} rightCount=${rightCount} max=${max} lastIndex=${lastIndex}`);
-  }
-
-  max = Math.max(max, leftCount + rightCount);
-
-  return max;
+  return Math.max(max, curMax);
 };
 
 export default {
