2e09686949523a8bd2a4937742da808dd8691160
minidonut <nycom13@gmail.com>
2020-06-02 20:37:42 +0900

    Solve twice #1

--- src/problems/1-two-sum/solution.ts
+++ src/problems/1-two-sum/solution.ts
@@ -1,15 +1,17 @@
 const Solution = (nums: number[], target: number): number[] => {
-  const cache: Record<number, number> = {};
+  const table: any = {};
+
   for (let i = 0; i < nums.length; i++) {
-    if (cache[nums[i]] !== undefined) {
-      return [cache[nums[i]], i];
+    if (nums[i] in table) {
+      return [table[nums[i]], i];
     } else {
-      cache[target - nums[i]] = i;
+      table[target - nums[i]] = i;
     }
   }
+  return [];
 };
 
 export default {
   "default": Solution,
-  validator: (arr: number[]) => arr.join(""),
+  validator: (arr: number[]) => arr.join(","),
 };
