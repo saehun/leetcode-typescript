c73c307a6d9576ff93ca666301b5eb8f8936e5db
minidonut <nycom13@gmail.com>
2020-06-03 06:01:10 +0900

    Solve twice #121

--- src/problems/121-best-time-to-buy-and-sell-stock/solution.ts
+++ src/problems/121-best-time-to-buy-and-sell-stock/solution.ts
@@ -6,9 +6,20 @@
  * O(n) solution
  */
 const maxProfit = (prices: number[]): number => {
+  let max = 0;
+  let buy = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    const price = prices[i];
+    if (price < buy) {
+      buy = price;
+    } else {
+      max = Math.max(max, price - buy);
+    }
+  }
+  return max;
 };
 
 export default {
   "default": maxProfit,
-  maxProfit,
+  validator: (x: any) => x,
 };
