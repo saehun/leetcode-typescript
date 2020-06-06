bd387883c46e85eae1c57db099f63d3ccc48ef3b
minidonut <nycom13@gmail.com>
2020-06-07 04:31:58 +0900

    Fail to Solve problem #5 in O(n)

--- src/problems/5-longest-palindromic-substring/case.ts
+++ src/problems/5-longest-palindromic-substring/case.ts
@@ -1,8 +1,8 @@
 export default [
-  { input: ["babad"], output: "bab" },
-  { input: ["cbbd"], output: "bb" },
-  { input: ["ccc"], output: "ccc" },
-  { input: ["bananas"], output: "anana" },
-  { input: ["ccd"], output: "cc" },
+  /* { input: ["babad"], output: "bab" }, */
+  /* { input: ["cbbd"], output: "bb" }, */
+  /* { input: ["ccc"], output: "ccc" }, */
+  /* { input: ["bananas"], output: "anana" }, */
+  /* { input: ["ccd"], output: "cc" }, */
   { input: ["ababababababa"], output: "ababababababa" },
 ];
--- src/problems/5-longest-palindromic-substring/solution.ts
+++ src/problems/5-longest-palindromic-substring/solution.ts
@@ -3,7 +3,7 @@
 
 
 // Expand center
-const Solution = (s: string): string => {
+const ExpandCenter = (s: string): string => {
   let palindrom = "";
 
   const expandCenter = (index: number) => {
@@ -32,6 +32,48 @@ const Solution = (s: string): string => {
   }
 
   return palindrom;
+};
+
+
+const Solution = (s: string): string => {
+  const arr: [number, boolean][] = [[0, true]]; // arr[x][1] === true when composed with single character
+
+  for (let i = 1; i < s.length; i++) {
+    const [matchedIndex, homoChar] = arr[i - 1];
+    if (homoChar) { // c, ccc, ccccc
+      if (s[i] === s[matchedIndex - 1]) {
+        arr.push([matchedIndex - 1, false]);
+      } else if (s[i] === s[matchedIndex]) {
+        arr.push([matchedIndex, true]);
+      } else {
+        arr.push([i, true]);
+      }
+    } else {
+      if (s[i] === s[matchedIndex - 1]) {
+        arr.push([matchedIndex - 1, false]);
+      } else if (s[i] === s[i - 1]) {
+        arr.push([i - 1, true]);
+      } else if (s[i] === s[i - 2]) {
+        arr.push([i - 2, false]);
+      } else {
+        arr.push([i, true]);
+      }
+    }
+  }
+
+  console.log(arr);
+  let max = [0, 0];
+  for (let i = 0; i < s.length; i++) {
+    /* console.log(`length is ${i - arr[i][0] + 1}, prev is ${max[0]} at index ${i}`); */
+    const length = i - arr[i][0] + 1;
+    if (length > max[0]) {
+      max = [length, i];
+      /* console.log(`max refreshed, ${max} `); */
+    }
+  }
+  console.log(max);
+
+  return s.slice(max[1] - max[0] + 1, max[1] + 1);
 };
 
 export default {
