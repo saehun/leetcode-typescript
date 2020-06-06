c13d4fd0b70945a8ba02a945b3731ddff0f8cf21
minidonut <nycom13@gmail.com>
2020-06-07 08:09:40 +0900

    Solve problem #322 multiple solution

--- src/problems/322-coin-change/solution.ts
+++ src/problems/322-coin-change/solution.ts
@@ -10,6 +10,9 @@ const Solution = (coins: number[], amount: number): number => {
       } else {
         arr[i + coin] = Math.min(used + 1, arr[i + coin]);
       }
+      if (i === coin) {
+        break;
+      }
     }
   }
 
@@ -17,7 +20,40 @@ const Solution = (coins: number[], amount: number): number => {
   return arr[amount];
 };
 
+const Solution2 = (coins: number[], amount: number): number => {
+  coins.sort((a, b) => b - a);
+  let min = amount + 1;
+  function dfs(index: number, target: number, count: number) {
+    console.log(`dfs(${index}, ${target}, ${count})`);
+    if (index === coins.length) return;
+    const curr = coins[index];
+    if (target % curr === 0) {
+      const res = count + target / curr;
+      return min = Math.min(min, res);
+    }
+    for (let i = Math.floor(target / curr); i >= 0 && count + i < min; i--) {
+      dfs(index + 1, target - i * curr, count + i);
+    }
+  }
+  dfs(0, amount, 0);
+  return min <= amount ? min : -1;
+};
+
+const Solution3 = (coins: number[], amount: number): number => {
+  const arr = Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
+  arr[0] = 0;
+  for (let i = 0; i <= amount; i++) {
+    for (const coin of coins) {
+      if (coin <= i) {
+        arr[i] = Math.min(arr[i - coin] + 1, arr[i]);
+      }
+    }
+  }
+
+  return arr[amount] > amount ? -1 : arr[amount];
+};
+
 export default {
-  "default": Solution,
+  "default": Solution3,
   validator: (x: any) => x,
 };
