ced2431f61db347fafee0534caa97b7747ef7519
minidonut <nycom13@gmail.com>
2020-06-07 05:16:41 +0900

    Solve problem #53 DP (four times)

--- src/problems/53-maximum-subarray/solution.ts
+++ src/problems/53-maximum-subarray/solution.ts
@@ -1,4 +1,4 @@
-const Solution = (nums: number[]): number => {
+const Simple = (nums: number[]): number => {
   let max = Number.MIN_SAFE_INTEGER;
   let buffer = 0;
   for (const num of nums) {
@@ -13,7 +13,29 @@ const Solution = (nums: number[]): number => {
   return max;
 };
 
+const DP = (nums: number[]): number => {
+  const dp = [...nums];
+
+  for (let i = 1; i < nums.length; i++) {
+    if (dp[i - 1] < 0) continue;
+    dp[i] += dp[i - 1];
+  }
+
+  return dp.reduce((acc, n) => Math.max(acc, n), Number.MIN_SAFE_INTEGER);
+};
+
+const DP2 = (nums: number[]): number => {
+  const dp = [nums[0]];
+
+  for (let i = 1; i < nums.length; i++) {
+    dp.push(dp[i - 1] < 0 ? nums[i] : nums[i] + dp[i - 1]);
+  }
+  console.log(dp);
+
+  return dp.reduce((acc, n) => Math.max(acc, n), Number.MIN_SAFE_INTEGER);
+};
+
 export default {
-  "default": Solution,
+  "default": DP2,
   validator: (x: any) => x,
 };
