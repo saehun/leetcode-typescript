5382a705791bbc46a521cd5f731fe44a62d61c2e
minidonut <nycom13@gmail.com>
2020-06-08 08:44:37 +0900

    Solve problem #66

--- src/problems/66-plus-one/solution.ts
+++ src/problems/66-plus-one/solution.ts
@@ -1,4 +1,6 @@
-const Solution = () => { };
+const Solution = (digits: number[]): number[] => {
+  return (BigInt(digits.join("")) + BigInt(1)).toString().split("").map(x => Number(x));
+};
 
 export default {
   "default": Solution,
