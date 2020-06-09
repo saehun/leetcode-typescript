0c2c1b3e710af8928891328075f4cd5f15c56a1e
minidonut <nycom13@gmail.com>
2020-06-10 08:13:19 +0900

    Solve problem #1249

--- src/problems/1249-minimum-remove-to-make-valid-parentheses/solution.ts
+++ src/problems/1249-minimum-remove-to-make-valid-parentheses/solution.ts
@@ -1,4 +1,41 @@
-const Solution = () => { };
+function Solution(s: string): string {
+  let parens = 0;
+  let tmp = "";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      parens++;
+      tmp += s[i];
+    } else if (s[i] === ")") {
+      if (parens > 0) {
+        tmp += s[i];
+        parens--;
+      } else {
+        continue;
+      }
+    } else {
+      tmp += s[i];
+    }
+  }
+
+  parens = 0;
+  let result = "";
+  for (let i = tmp.length - 1; i >= 0; i--) {
+    if (tmp[i] === ")") {
+      parens++;
+      result += tmp[i];
+    } else if (tmp[i] === "(") {
+      if (parens > 0) {
+        result += tmp[i];
+        parens--;
+      } else {
+        continue;
+      }
+    } else {
+      result += tmp[i];
+    }
+  }
+  return result.split("").reverse().join("");
+};
 
 export default {
   "default": Solution,
