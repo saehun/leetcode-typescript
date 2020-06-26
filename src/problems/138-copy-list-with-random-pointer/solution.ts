eadf0cc6f1b96a0fddff620d2c9b436e9360b0b7
minidonut <nycom13@gmail.com>
2020-06-26 17:29:20 +0900

    Solve problem #138

--- src/problems/138-copy-list-with-random-pointer/solution.ts
+++ src/problems/138-copy-list-with-random-pointer/solution.ts
@@ -1,6 +1,40 @@
-const Solution = () => { };
+type Node = {
+  val: number;
+  next: Node;
+  random: Node;
+  index?: number;
+  randomIndex?: number;
+}
+
+const copyRandomList = function(head: Node) {
+  let cur = head;
+  let i = 0;
+  const list: Node[] = [];
+  while (cur !== null) { // mark index
+    cur.index = i++;
+    list.push({ val: cur.val, next: null, random: null });
+    cur = cur.next;
+  }
+
+  cur = head;
+  while (cur !== null) { // mark index
+    if (cur.random) {
+      cur.randomIndex = cur.random.index;
+    }
+    cur = cur.next;
+  }
+
+  cur = head;
+  while (cur !== null) {
+    list[cur.index].next = list[cur.index + 1] || null;
+    list[cur.index].random = list[cur.randomIndex] || null;
+
+    cur = cur.next;
+  }
+  return list[0];
+};
 
 export default {
-  "default": Solution,
+  "default": copyRandomList,
   validator: (x: any) => x,
 };
