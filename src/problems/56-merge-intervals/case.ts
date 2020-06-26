b8c5f97358bd58001ca3f6fe4fb1813aabdf8334
minidonut <nycom13@gmail.com>
2020-06-26 16:52:48 +0900

    Solve problem #56

--- src/problems/56-merge-intervals/case.ts
+++ src/problems/56-merge-intervals/case.ts
@@ -1,3 +1,4 @@
 export default [
-  { input: [0], output: true },
+  { input: [[[1, 3], [2, 6], [8, 10], [15, 18]]], output: [[1, 6], [8, 10], [15, 18]] },
+  { input: [[[1, 4], [4, 5]]], output: [[1, 5]] },
 ];
--- src/problems/56-merge-intervals/solution.ts
+++ src/problems/56-merge-intervals/solution.ts
@@ -1,6 +1,33 @@
-const Solution = () => { };
+/**
+ * nlogn이 최선인가..
+   다른 예시들에서도 nlogn이 최선이라 하고 있긴 하다.
+ */
 
+function merge(intervals: number[][]): number[][] {
+  if (intervals.length === 0) return []; // exclude special case
+  intervals = intervals.sort((a, b) => a[0] - b[0]);
+  let start = intervals[0][0];
+  let end = intervals[0][1];
+  const merged = [];
+  for (let i = 1; i < intervals.length; i++) {
+    const interval = intervals[i];
+    if (interval[0] <= end) {
+      if (interval[1] > end) {
+        end = interval[1];
+      } else {
+        //pass
+      }
+    } else {
+      merged.push([start, end]);
+      start = interval[0];
+      end = interval[1];
+    }
+  }
+  merged.push([start, end]);
+
+  return merged;
+}
 export default {
-  "default": Solution,
-  validator: (x: any) => x,
+  "default": merge,
+  validator: (x: number[][]) => x.flat().join(","),
 };
