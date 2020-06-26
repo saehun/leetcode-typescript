927398dbc54c5f9b37624ee706318d1cc0d32fe6
minidonut <nycom13@gmail.com>
2020-06-26 14:30:01 +0900

    Solve problem #200

--- src/problems/200-number-of-islands/solution.ts
+++ src/problems/200-number-of-islands/solution.ts
@@ -1,7 +1,33 @@
 const Solution = (grid: string[][]) => {
+  let num = 0;
+  for (let i = 0; i < grid.length; i++) {
+    const row = grid[i];
+    for (let j = 0; j < row.length; j++) {
+      const cell = row[j];
+      if (cell === "0") continue; // skip zero
+      if (cell === "2") continue; // skip marked
+      mark(i, j);
+      num++;
+    }
+  }
 
-  console.log(grid);
-  return 1;
+  function mark(row: number, col: number) {
+    if (
+      row < 0 ||
+      row >= grid.length ||
+      col < 0 ||
+      col >= grid[0].length ||
+      grid[row][col] === "2" ||
+      grid[row][col] === "0"
+    ) return;
+    grid[row][col] = "2";
+    mark(row - 1, col);
+    mark(row + 1, col);
+    mark(row, col - 1);
+    mark(row, col + 1);
+  }
+
+  return num;
 };
 
 export default {
